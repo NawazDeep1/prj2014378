afd233f26007067f07107e580e1c7831852c035a
Nawaz Deep <nawazdeep@gmail.com>
2020-10-28 23:28:44 -0400

    CSS is ready

--- PhpFunctions/php.php
+++ PhpFunctions/php.php
@@ -1,46 +1,56 @@
 <?php
-
-define("CSS_FOLDER","CSS/");
-define('CSSFILE',CSS_FOLDER."Css.css");
+    
+#create constants
+define("FOLDER_CSS","CSS/");
+define("CSSFILE",FOLDER_CSS."Css.css"); 
 define("IMAGES","Images/");
-define("LOGO",IMAGES."front.wep");
-
-
-function Header($Heading)
-{
+define("LOGO",FOLDER_IMAGES."front.webp");
+define("HOME_PAGE","Home.php");
+define("BUY_PAGE","");
+define("ORDER_PAGE","");
+//define("PRODUCT_EX_BIKE",FOLDER_IMAGES."Excerscise Bike.jpeg");
+//define("PRODUCT_TREDMILL",FOLDER_IMAGES."Tredmill.jpg");
+//define("PRODUCT_ELLIPTICAL",FOLDER_IMAGES."Elliptical.jpg");
+//define("PRODUCT_CYCLE",FOLDER_IMAGES."Cycle.jpg");
+//define("PRODUCT_ROWERS",FOLDER_IMAGES."Rowers.jpg");
+$products = array(PRODUCT_EX_BIKE, PRODUCT_TREDMILL, PRODUCT_ELLIPTICAL, PRODUCT_CYCLE, PRODUCT_ROWERS);
+function pageHeader($title)
+    {
     ?>
-      <!DOCTYPE>
-        <html>
-            <head>
-                <meta charset ="UTF-8">
-                <title>
-                    <?php 
-                    echo $Heading;
-                    ?>
-                </title>
-                <link rel="stylesheet" type="text/css" href="<?php echo CSSFILE; ?>">
-            </head>
-        
-<body>
-<?php
-}
-
-function Footer()
-{
+    <!DOCTYPE>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title;?></title>
+        <link rel="stylesheet" type="text/css" href ="<?php echo CSS_FILE;?>">
+    </head>
+    <body> 
+    <?php
+    }
+    function pageFooter()
+    {
+        copyright();
     ?>
-
-</body>
-</html>
-<?php
-}
-function Copyright()
-{
-    echo"<br> Copyright NawazDeep Singh (2014378)".date('y')."<br>";
-}
-
-function Logo()
-{
-    echo'<img src = "'.front.'" width = "250px" height ="170px">';
-}
-
-
+    </body>
+    </html>   
+    <?php
+    }
+    function copyright()
+    {
+        echo '<br><p class = "copyright">Copyright Nawaz Deep (2013417) '.date('Y').'</p>';
+    }
+    function displayLogo()
+    {
+        echo '<a href = "'.HOME_PAGE.'"><img src = "'.LOGO.'" height = "150px" width = "230px" class="logo"></a>';
+    }
+    function navigationMenu()
+    {
+        echo '<div class = "navMenu">';
+        displayLogo();
+        echo '<ul>';
+        echo '<li><a href = "'.HOME_PAGE.'">Home</a></li>'; 
+        echo '<li><a href = "'.BUY_PAGE.'">Buy</a></li>';
+        echo '<li><a href = "'.ORDER_PAGE.'">Orders</a></li>';
+        echo '</ul>';
+        echo '</div>';
+    }
--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
-
-
-        include_once 'PhpFunction/php.php';
-        Header("Hello");
-        ?>
-<br>Hello
+    include_once 'PhpFunctions/php.php';
+    pageHeader("Hello");
+    navigationMenu();
+    shuffle($products);
+    ?>
+<h1 class="info">Supreme Fitness is the distributer of Fitness Equipment. We sell best-in-class exercise machines for the home and for a wide variety of commercial facilities</h1>
 <?php
-        Footer();
-        ?>
+    if($products[0] == PRODUCT_CYCLE)
+    {
+        echo '<br><img src="'.$products[0].'" class="product_cycle"></img>';
+    }
+    else
+    {
+        echo '<br><img src="'.$products[0].'" class="product"></img>';
+    }
+    pageFooter();
+?>
